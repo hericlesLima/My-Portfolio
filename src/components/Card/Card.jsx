196aaf4dfc4d8ef88882e4dcd08f20bab5568bd1
hericlesLima <hericlesrecheado@gmail.com>
2021-09-27 09:36:51 -0100

    card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Card/Card.scss";
+
+export default function Card(props) {
+  return (
+    <div className="card">
+      <div>
+        <h1>{props.title}</h1>
+      </div>
+      <div>
+        <p className="card-txt">
+            {props.text}
+        </p>
+      </div>
+      <div className="card-btn">
+        <a href={props.gtbLink}>Github</a>
+        <a href={props.link}>Link</a>
+      </div>
+    </div>
+  );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Card from "../components/Card/Card";
 import Footer from "../components/Footer/Footer";
 
 export default function MainPage() {
     return(
         <div>
-            Main Page
-            <Footer/>
+            <Card title="AC-Defender" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pretium velit nec diam pretium tempor. Vivamus viverra velit sit amet risus finibus cursus. Aenean ipsum diam, ullamcorper et justo in" />
         </div>
     )
 }
