48356b6996099b4fed5b6c9df738f2e31f5e6bbc
hericlesLima <hericlesrecheado@gmail.com>
2021-09-27 00:44:03 -0100

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../Footer/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-details">
+          <p className="footer-title">Social</p>
+          <a href="https://www.facebook.com/lmntor/" target="_blank" className="footer-link">Facebook</a>
+          <a href="https://www.linkedin.com/in/hericles-lima/" target="_blank" className="footer-link">Linkedin</a>
+        </div>
+        <div className="separator"></div>
+        <div className="footer-details">
+          <p className="footer-title">E-mail</p>
+          <p>hericlesrecheado@gmail.com</p>
+          <p>hericlesrecheado@outlook.com</p>
+        </div>
+        <div className="separator"></div>
+        <div className="footer-details">
+          <p className="footer-title">Phone's</p>
+          <p>+238 977 68 95</p>
+          <p>+238 977 68 95</p>
+        </div>
+      </div>
+      <div className="copyright footer-title">
+        <p>Copyright @ 2021 | Powered by: Héricles Recheado</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Footer from "../components/Footer/Footer";
+
+export default function MainPage() {
+    return(
+        <div>
+            Main Page
+            <Footer/>
+        </div>
+    )
+}
